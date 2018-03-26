9a938852890e6a306f58e664d43342d0375630a5
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-03-26 15:20:40 -0700

    create validations for auction model

--- api/app/models/auction.rb
+++ api/app/models/auction.rb
@@ -1,2 +1,22 @@
 class Auction < ApplicationRecord
+    has_many :bids, dependent: :destroy
+
+    validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+    validates :description, presence: true, length: { minimum: 10, maximum: 3000 }
+    validates :reserve_price, numericality: { greater_than_or_equal_to: 0 }
+    validate :expiry_cannot_be_in_past
+    before_save :capitalize_title
+
+    private
+
+    def capitalize_title
+        title.capitalize!
+    end
+
+    def expiry_cannot_be_in_past
+        if expiry_date.present? && expiry_date <= Date.today
+            errors.add :expiry_date, "can't be in the past"
+        end
+    end
+
 end
