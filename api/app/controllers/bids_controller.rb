928743f887a3c6793b9ac2c4e856a0fcffd7803f
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-03-26 16:52:08 -0700

    create actions for bids controller

--- api/app/controllers/bids_controller.rb
+++ api/app/controllers/bids_controller.rb
@@ -1,10 +1,19 @@
 class BidsController < ApplicationController
 
     def create
+        bid = Bid.new bid_params
+        bid.auction = Auction.find params[:auction_id]
+        bid.save
     end
 
     def destroy
+        bid = Bid.find params[:id]
+        bid.destroy
     end
 
-    
+    private
+
+    def bid_params
+        params.require(:bid).permit :price
+    end
 end
