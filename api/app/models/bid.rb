f8c86bfddcd10695436b6a4b6111571e026ca90a
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-03-26 15:54:56 -0700

    create validations for bid model

--- api/app/models/bid.rb
+++ api/app/models/bid.rb
@@ -1,2 +1,17 @@
 class Bid < ApplicationRecord
+    belongs_to :auction
+
+    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+    validate :be_greater_than_reserve_price
+
+    private
+
+    def be_greater_than_reserve_price
+        reserve_price = auction.reserve_price
+        if price < reserve_price
+            errors.add :price, "cannot be less than current price"
+        end
+    end
+
 end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326211243) do
+ActiveRecord::Schema.define(version: 20180326224622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 20180326211243) do
     t.decimal "price", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "auction_id"
+    t.index ["auction_id"], name: "index_bids_on_auction_id"
   end
 
+  add_foreign_key "bids", "auctions"
 end
