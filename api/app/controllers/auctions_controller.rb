8ae0979be8b6a04efa02ace8d7395c376e39f643
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-03-26 16:44:38 -0700

    create actions for auctions_controller

--- api/app/controllers/auctions_controller.rb
+++ api/app/controllers/auctions_controller.rb
@@ -1,24 +1,47 @@
 class AuctionsController < ApplicationController
+    before_action :find_auction, only: [:update, :show, :destroy]
 
     def index
-    end
-
-    def new
+        auctions = Auction.order created_at: :desc
+        render json: auctions
     end
 
     def create
-    end
-
-    def edit
+        auction = Auction.new auction_params
+        if auction.save
+            render json: {id: auction.id}
+        else
+            head :conflic
+        end
     end
 
     def update
+        if auction.update auction_params
+            render json: {id: auction.id}
+        else
+            head :conflict
+        end
     end
 
     def show
+        render json: auction
     end
 
     def destroy
+        auction.destroy
     end
     
+
+    private
+
+    def auction_params
+        params.require(:auction)
+        .permit(:title, :description, :reserve_price, :expiry_date)
+    end
+
+    def find_auction
+        auction = auction.find params[:id]
+    end
+
+
 end
