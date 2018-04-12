3b96fd9b9a8eb25db28243e65426c77bee1479a5
lastDayIn2017 <lastDayIn2017@gmail.com>
2018-04-12 04:20:34 -0700

    implement mailer with gmail

--- api/config/initializers/active_mailer.rb
+++ api/config/initializers/active_mailer.rb
@@ -1,1 +1,10 @@
-asdf
+ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['EMAIL_USERNAME'],
+    password: ENV['EMAIL_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+}
+
+# http://guides.rubyonrails.org/action_mailer_basics.html#action-mailer-configuration-for-gmail
